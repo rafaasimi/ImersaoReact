367232b5b58e47df75583c9bdf93c929de2e24e1
rafaasimi <rafaasimi@hotmail.com>
2020-07-27 11:02:04 -0300

    add initial files

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# ImersaoReact
-Criação do AluraFlix, app web utilizando React na semana da #ImersãoReact da Alura.
+<p align='center'><img width='200' src="/images/stormtrooper.png"/></p>
+<h1 align='center'>AluraFlix - #ImersãoReact</h1>
+
+<p align='center'>
+<img src="https://img.shields.io/github/repo-size/rafaasimi/ImersaoReact">
+<img src="https://img.shields.io/github/last-commit/rafaasimi/ImersaoReact">
+<img src="https://img.shields.io/github/license/rafaasimi/ImersaoReact">
+</p>
+
+<h3>🔖 Descrição</h3>
+<p>Criação do AluraFlix, app web utilizando React na semana da #ImersãoReact da Alura.<p>
+
+
+<h3>📝 Licença</h3>
+<p>O projeto se encontra sob licença MIT. Para mais detalhes, acesse <a href='LICENSE'>license<a>.</p>
+<p>Criado com 💙 por <a href='https://github.com/rafaasimi/' target='blank'>Rafael Simionato</a></p>
+
---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import Menu from './components/Menu'
+import dadosIniciais from './data/dados_iniciais.json'
+import BannerMain from './components/BannerMain'
+import Carousel from './components/Carousel'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <div style={{background: "#141414" }}>
+      <Menu/>
+
+      <BannerMain
+        videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+        url={dadosIniciais.categorias[0].videos[0].url}
+        videoDescription={"O que é Front-End? Trabalhando na area"}
+      />
+
+      <Carousel
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[0]}
+      />
+
+      <Carousel
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[1]}
+      />
+
+      <Carousel
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[2]}
+      />
+
+      <Carousel
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[3]}
+      />
+
+      <Carousel
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[4]}
+      />
+
+      <Carousel
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[5]}
+      />
+
+    </div>
+  );
+}
+
+export default App;
