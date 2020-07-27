e29b1a9fce95a4f864ab990fb0a5ccfbd1fcecc7
rafaasimi <rafaasimi@hotmail.com>
2020-07-27 11:42:47 -0300

    change video description

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <BannerMain
         videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
         url={dadosIniciais.categorias[0].videos[0].url}
-        videoDescription={"O que é Front-End? Trabalhando na area"}
+        videoDescription={"Highlights de Counter-Strike: Global Offensive"}
       />
 
       <Carousel
